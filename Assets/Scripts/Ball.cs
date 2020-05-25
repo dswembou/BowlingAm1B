519226eea013750f95c272db5cf2536fc95df7bc
DaraDannies <dvanwakeren@gmail.com>
2020-05-25 11:17:27 +0200

    eind les 3

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Ball : MonoBehaviour
+{
+    private Vector3 launchVelocity;
+    private Rigidbody _rigidbody;
+    private AudioSource _audioSource;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _audioSource = GetComponent<AudioSource>();
+        _rigidbody.useGravity = false;
+        launchVelocity = new Vector3(0,0,200f); //tijdelijke snelheid dat de bal wordt gegooid
+        //Launch(launchVelocity);
+    }
+
+    public void Launch(Vector3 launchVelocity)
+    {
+        _rigidbody.useGravity = true;
+        _rigidbody.velocity = launchVelocity;
+        _audioSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/DragLaunch.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Jobs;
+
+[RequireComponent(typeof(Ball))]
+public class DragLaunch : MonoBehaviour
+{
+    private Ball _ball;
+
+    private Vector3 _dragStart, _dragEnd;
+    private float _startTime, _endTime;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _ball = GetComponent<Ball>();
+    }
+
+    public void MoveStart(float xNudge)
+    {
+        transform.Translate(xNudge, 0f, 0f);
+    }
+
+    // Update is called once per frame
+    public void DragStart()
+    {
+        _dragStart = Input.mousePosition;
+        _startTime = Time.time;
+    }
+
+    public void DragEnd()
+    {
+        _dragEnd = Input.mousePosition;
+        _endTime = Time.time;
+
+        float _dragDuration = _endTime - _startTime;
+
+        float _launchSpeedX = (_dragEnd.x - _dragStart.x) / _dragDuration;
+        float _launchSpeedZ = (_dragEnd.y - _dragStart.y) / _dragDuration;
+        
+        Vector3 launchVelocity = new Vector3(_launchSpeedX, 0f, _launchSpeedZ);
+
+        _ball.Launch(launchVelocity);
+    }
+}
