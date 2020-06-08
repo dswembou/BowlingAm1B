7e5ab84a7cb45609a6ef8c5df96f13b9d80136eb
DaraDannies <dvanwakeren@gmail.com>
2020-06-08 13:49:49 +0200

    maandag 8 juni

---
+++ Assets/Scripts/Pin.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+
+public class Pin : MonoBehaviour
+{
+    public float standingTreshold = 3f; //afwijking dat de Pin mag hebben bij staan
+
+    // Update is called once per frame
+    void Update()
+    {
+        print(name + IsStanding()); //test om te zien welke Pin staat en welke niet
+    }
+
+    public bool IsStanding()
+    {
+        UnityEngine.Vector3 rotationInEuler = transform.rotation.eulerAngles;
+
+        float tiltInX = Mathf.Abs(rotationInEuler.x);
+        float tiltInZ = Mathf.Abs(rotationInEuler.z);
+
+        if(tiltInX < standingTreshold && tiltInZ < standingTreshold)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
---
+++ Assets/Scripts/PinSetter.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PinSetter : MonoBehaviour
+{
+    [SerializeField] Text standingDisplay;
+
+    private bool ballEnteredBox = false;
+
+    int CountStanding()
+    {
+        int standing = 0;
+
+        foreach (Pin pin in GameObject.FindObjectsOfType<Pin>())
+        {
+            if (pin.IsStanding())
+            {
+                standing++;
+            }
+        }
+
+        return standing;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        standingDisplay.text = CountStanding().ToString();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Ball>())
+        {
+            ballEnteredBox = true;
+            standingDisplay.color = Color.red;
+            print(ballEnteredBox);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        GameObject thingLeft = other.gameObject;
+
+        if (thingLeft.GetComponentInParent<Pin>())
+        {
+            print("pin left");
+            Destroy(thingLeft);
+        }
+    }
+}
