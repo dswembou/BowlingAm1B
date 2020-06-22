0f03d6b1fb7520f30acce958ddaa8e16f00468ee
DaraDannies <dvanwakeren@gmail.com>
2020-06-22 10:30:40 +0200

    aan de slag met TestRunner

---
+++ Assets/Scripts/ActionManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionManager
+{
+        
+    public enum Action { Tidy, Reset, Endturn, EndGame, Undefined };
+    public Action Bowl(List<int> pins)
+    {
+        Action nextAction = Action.Undefined;
+
+        for (int i = 0; i < pins.Count; i++)
+        {
+            if (i == 21)
+            {
+                nextAction = Action.EndGame;
+            }
+            else if (i == 20)
+            {
+                if(pins[i] == 10)
+                {
+                    nextAction = Action.Reset;
+                }
+                
+            }
+            else if (i % 2 == 0) //Eerste bal van een beurt 
+            {
+                if (pins[i] == 10)
+                {
+                    pins.Insert(i, 0); //Maak een virtuele 0 aan na een strike
+                    nextAction = Action.Endturn;
+                }
+                else
+                {
+                    nextAction = Action.Tidy;
+                }
+
+            }
+            else //Tweede bal van een beurt
+            {
+                nextAction = Action.Endturn;
+            }
+        }
+        return nextAction;
+    }
+}
+
+
---
+++ Assets/Tests/ActionManagerTest.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class ActionManagerTest
+    {
+        private ActionManager actionManager;
+        private ActionManager.Action endTurn = ActionManager.Action.Endturn;
+        private ActionManager.Action tidy = ActionManager.Action.Tidy;
+        private ActionManager.Action reset = ActionManager.Action.Reset;
+
+        [SetUp]
+        public void Setup()
+        {
+            actionManager = new ActionManager();
+        }
+        
+        [Test]
+        public void PassingTest()
+        {
+            // Use the Assert class to test conditions
+            Assert.AreEqual(1, 1);
+        }
+        [Test]
+        public void T01StrikeReturnsEndTurn()
+        {
+            int[] rolls = { 10 };
+            Assert.AreEqual(endTurn, actionManager.Bowl(rolls.ToList()));
+        }
+        [Test]
+        public void T02Pin9ReturnsTidy()
+        {
+            int[] rolls = { 9 };
+            Assert.AreEqual(tidy, actionManager.Bowl(rolls.ToList()));
+        }
+        [Test]
+        public void T03Pin24ReturnsEndTurn()
+        {
+            int[] rolls = { 2 , 4 };
+            Assert.AreEqual(endTurn, actionManager.Bowl(rolls.ToList()));
+        }
+        [Test]
+        public void T04ExtraTurnAfter20Strikes()
+        {
+            int[] rolls = { 1,1, 1,1 , 1,1 , 1,1 , 1,1 , 1,1, 1,1 , 1,1 , 1,1 , 10,10 };
+            Assert.AreEqual(reset, actionManager.Bowl(rolls.ToList()));
+        }
+    }
+}
